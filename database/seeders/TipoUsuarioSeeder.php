aed2e91524e8a7c90ef196ef9ad75338b162a1b7
Augusto Britez <agussb52@gmail.com>
2025-08-03 22:14:45 -0300

    GPP-002-seeders

--- database/seeders/TipoUsuarioSeeder.php
+++ database/seeders/TipoUsuarioSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\TipoUsuario;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -15,16 +16,25 @@ class TipoUsuarioSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('tipo_usuario')->insert([
-            [
-                'nombre' => 'administrativo',
-            ],
-            [
-                'nombre' => 'empresa',
-            ],
-            [
-                'nombre' => 'estudiante',
-            ],
-        ]);
+        TipoUsuario::firstOrCreate(
+            ['nombre' => 'admin'],
+            ['nombre' => 'admin'],
+        );
+
+        TipoUsuario::firstOrCreate(
+            ['nombre' => 'administrativo'],
+            ['nombre' => 'administrativo'],
+        );
+
+        TipoUsuario::firstOrCreate(
+            ['nombre' => 'empresa'],
+            ['nombre' => 'empresa'],
+        );
+
+        TipoUsuario::firstOrCreate(
+            ['nombre' => 'estudiante'],
+            ['nombre' => 'estudiante'],
+        );
+        
     }
 }
